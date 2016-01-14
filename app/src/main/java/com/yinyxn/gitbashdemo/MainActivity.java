0ab0699f6180cdfe18dfb0bb579c8d5848561538
woals <798041778@qq.com>
2016-01-14 15:25:39 +0800

    Initial commit

---
+++ app/src/main/java/com/yinyxn/gitbashdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.yinyxn.gitbashdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
